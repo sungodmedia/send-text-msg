38d6244f5c1fb0f8cef6e10a8083c3f71153e4db
Susan Gleason <susanegleason@comcast.net>
2015-09-18 22:39:31 -0400

    single validate no error

--- app/models/send.rb
+++ app/models/send.rb
@@ -1,2 +1,6 @@
 class Send < ActiveRecord::Base
+
+	validates :message, :presence => true :message => "Text message is required"
+	validates :phone, :presence => true :message => "Phone number is required"
+	
 end
